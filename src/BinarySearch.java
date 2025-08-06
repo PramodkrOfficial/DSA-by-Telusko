03bdd1c8e218345ebf580e69470f49cd65fddd63
PramodkrOfficial <pramodkumarrupauliya@gmail.com>
2025-08-06 08:25:13 +0530

    Learning Phase I

---
+++ src/BinarySearch.java
@@ -0,0 +1,37 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+//        int nums[] = {5,7,9,11,13};
+        int nums[] = new int[32];
+        int target = 900;
+        
+        int result = binarySearch(nums, target);
+        
+        if(result != -1){
+            System.out.println("Element found at index: " + result);
+        }else
+            System.out.println("Element not found...");
+    }
+    
+    public static int binarySearch(int[] nums, int target) {
+
+        int steps = 0;
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right) {
+            steps++;
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target) {
+                System.out.println("Steps taken by Binary: " + steps);
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Steps taken by Binary: " + steps);
+        return -1;
+    }
+}
---
+++ src/BinarySearchWithRecursive.java
@@ -0,0 +1,29 @@
+public class BinarySearchWithRecursive {
+    public static void main(String[] args) {
+        int nums[] = {5,7,9,11,13};
+//        int nums[] = new int[1000];
+        int target = 9;
+
+        int result = binarySearch(nums, target, 0, nums.length-1);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        }else
+            System.out.println("Element not found..");
+
+    }
+
+    public static int binarySearch(int[] nums, int target, int left, int right) {
+
+        if(left <= right) {
+            int mid = (left + right)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                return binarySearch(nums, target, mid+1, right);
+            }else
+                return binarySearch(nums, target, left,mid-1);
+        }
+        return -1;
+    }
+}
